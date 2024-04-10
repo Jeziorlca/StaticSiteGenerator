939c6ad1b7187ae7b08651630a89ee2c71620f6d
Jezior <jeziorlca@gmail.com>
2024-04-10 12:05:15 +0200

    added LeafNode class and unittests

--- htmlnode.py
+++ htmlnode.py
@@ -9,7 +9,20 @@ class HTMLNode:
         raise NotImplementedError
     
     def props_to_html(self):
-        raise NotImplementedError
+        if self.props is None:
+            return ""
+        return " ".join(key+"="+value for key, value in self.props.items())
     
     def __repr__(self):
         return str(self.tag) + " ----- " + str(self.value) + " ----- " + str(self.children) + " ----- " + str(self.props)
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str = None, value: str = None, children: list = None, props: dict = None):
+        if value is None:
+            raise ValueError("Value cannot be None")
+        super().__init__(tag, value, children, props)
+    
+    def to_html(self):
+        if self.props_to_html() == "":
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
--- test_htmlnode.py
+++ test_htmlnode.py
@@ -1,8 +1,23 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_htmlnode(self):
         html = HTMLNode(tag="p", value="Hello World!", children=["Hello", "World!"], props={"href": "https://www.google.com"})
         print(html)
+    
+    def test_props_to_html(self):
+        html = HTMLNode(tag="p", value="Hello World!", children=["Hello", "World!"], props={"href": "https://www.google.com"})
+        html2 = HTMLNode(props={"href": "https://www.google.com", "target": "_blank" })
+        html3 = HTMLNode(props={"href": "https://www.google.com", "target": "_blank", "rel": "noopener noreferrer", "aria-label": "Open in a new tab" })
+        self.assertEqual(html.props_to_html(), "href=https://www.google.com")
+        self.assertEqual(html2.props_to_html(), "href=https://www.google.com target=_blank")
+        self.assertEqual(html3.props_to_html(), "href=https://www.google.com target=_blank rel=noopener noreferrer aria-label=Open in a new tab")
+
+class TestLeafNode(unittest.TestCase):
+    def test_leafnode(self):
+        html = LeafNode(tag="p", value="This is a paragraph of text.")
+        html2 = LeafNode(tag="a",value= "Click me!", props={"href": "https://www.google.com"})
+        self.assertEqual(html.to_html(), "<p>This is a paragraph of text.</p>")
+        self.assertEqual(html2.to_html(), "<a href=https://www.google.com>Click me!</a>")
